284795b2a6d5a11e37d35522917cefe65c320f88
Mike Galvin <galvi024@umn.edu>
2016-01-25 11:03:20 -0600

    initial commit

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,17 @@
 class Bottles
-
+  def verse(bottle_count)
+     "#{bottle_text(bottle_count)} of beer on the wall, " +
+      "#{bottle_count} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{bottle_count - 1} bottle#{bottle_count -1 == 1 ? '':'s'} of beer on the wall.\n"
+  end
+  def bottle_text(bottle_count)
+    if bottle_count == 1
+      "#{bottle_count} bottle"
+    elsif bar == 0
+      "no more bottles"
+    else
+      "#{bottle_count} bottles"
+    end
+  end
 end
